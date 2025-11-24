bb4a6caa68c69bd8928f52b29e6775c780c9f979
D.Bazhenov <dmitriy.bazhenov@gmail.com>
2025-11-24 17:45:33 +0100

    Added all pages

--- src/pages/Navigation.jsx
+++ src/pages/Navigation.jsx
@@ -1,24 +1,45 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home/Home";
-import Login from "../pages/Auth/Login/Login";
-import Register from "../pages/Auth/Register/Register";
-import NotFound from "../pages/NotFound/NotFound";
-import Reset from "../pages/Auth/Reset/Reset";
-import CookiesPolicy from "../pages/CookiesPolicy/CookiesPolicy";
-import PrivacyPolicy from "../pages/PrivacyPolicy/PrivacyPolicy";
-import Terms from "../pages/Terms/Terms";
+
+import NotFoundPage from "./NotFoundPage";
+
+import LoginPage from "./Auth/LoginPage";
+import RegisterPage from "./Auth/SignupPage";
+import ConfirmPage from "./Auth/ConfirmPage";
+import ResetPasswordPage from "./Auth/ResetPasswordPage";
+
+import HomePage from "./HomePage";
+import ExplorePage from "./ExplorePage/index";
+import MessagesPage from "./MessagesPage";
+import ProfilePage from "./ProfilePage";
+
+import CookiesPolicyPage from "./CookiesPolicyPage";
+import PrivacyPolicyPage from "./PrivacyPolicyPage";
+import TermsPage from "./TermsPage";
+import LearnMorePage from "./LearnMorePage";
+import EditProfilePage from "./ProfilePage/EditProfilePage";
 
 const Navigation = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/reset" element={<Reset />} />
-      <Route path="/cookies" element={<CookiesPolicy />} />
-      <Route path="/privacypolicy" element={<PrivacyPolicy />} />
-      <Route path="/terms" element={<Terms />} />
-      <Route path="*" element={<NotFound />} />
+      <Route>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/reset" element={<ResetPasswordPage />} />
+        <Route path="/confirm" element={<ConfirmPage />} />
+        <Route path="/learn-more" element={<LearnMorePage />} />
+      </Route>
+      <Route>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:id" element={<ProfilePage />} />
+        <Route path="/:id/edit" element={<EditProfilePage />} />
+        <Route path="/direct" element={<MessagesPage />} />
+        <Route path="/direct/:id" element={<MessagesPage />} />
+        <Route path="/" element={<ExplorePage />} />
+      </Route>
+      <Route path="/cookies" element={<CookiesPolicyPage />} />
+      <Route path="/privacypolicy" element={<PrivacyPolicyPage />} />
+      <Route path="/terms" element={<TermsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 };
