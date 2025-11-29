19312bf7e6d7a8877cb6b65372adf3bcdd449fdb
D.Bazhenov <dmitriy.bazhenov@gmail.com>
2025-11-29 21:02:11 +0100

    Added mock-data for the Search modal

--- src/modules/Search/Search.jsx
+++ src/modules/Search/Search.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 
 import TextField from "../../shared/components/TextField/TextField";
 import Avatar from "../../shared/components/Avatar/Avatar";
+import testUserAvatar from "../../assets/images/test-user.jpg";
 
 import styles from "./Search.module.css";
 
 const recentProfiles = [
   {
     username: "sashaa",
+    avatar: testUserAvatar,
   },
 ];
 
@@ -38,7 +40,7 @@ const Search = () => {
         <ul className={styles.recentList}>
           {recentProfiles.map((profile) => (
             <li key={profile.username} className={styles.recentItem}>
-              <Avatar size="sm" />
+              <Avatar size="sm" src={profile.avatar} alt={profile.username} />
               <div className={styles.userInfo}>
                 <span className={styles.username}>{profile.username}</span>
               </div>
