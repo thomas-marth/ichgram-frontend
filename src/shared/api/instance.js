119f6ddfdd92b3b3e8ad60ff580372267bab9403
D.Bazhenov <dmitriy.bazhenov@gmail.com>
2025-12-03 10:58:00 +0100

    Changed instance.js

--- src/shared/api/instance.js
+++ src/shared/api/instance.js
@@ -2,10 +2,11 @@ import axios from "axios";
 
 // console.log(import.meta.env);
 
-const { VITE_API_URL: baseURL } = import.meta.env;
+const { VITE_API_URL } = import.meta.env;
+const baseApiURL = VITE_API_URL ? `${VITE_API_URL}/api` : "/api";
 
 const authInstance = axios.create({
-  baseURL: `${baseURL}/api`,
+  baseURL: baseApiURL,
   withCredentials: true,
 });
 
