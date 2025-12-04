4f2ef2ca433ebda8ae81c6300b7e266ece443e90
D.Bazhenov <dmitriy.bazhenov@gmail.com>
2025-12-04 14:37:51 +0100

    Updated instance.js

--- src/modules/Login/Login.jsx
+++ src/modules/Login/Login.jsx
@@ -1,14 +1,25 @@
+import { useSelector, useDispatch } from "react-redux";
+import { Navigate, Link } from "react-router-dom";
+
+import { loginUser } from "./../../redux/auth/authThunks";
+import { selectAuthRequest } from "../../redux/auth/authSelectors";
+
 import Container from "../../shared/components/Container/Container";
 import CardFooter from "../../shared/components/CardFooter/CardFooter";
 import LoginForm from "./LoginForm/LoginForm";
-
-import { Link } from "react-router-dom";
 
 import Logo from "./../../shared/components/Logo/Logo";
 import loginImg from "../../assets/images/loginImg.png";
 import styles from "./Login.module.css";
 
 const Login = () => {
+  const { error, loading } = useSelector(selectAuthRequest);
+  const dispatch = useDispatch();
+
+  const onLogin = async (payload) => {
+    dispatch(loginUser(payload));
+  };
+
   return (
     <Container>
       <div className={styles.container}>
@@ -25,7 +36,8 @@ const Login = () => {
               <Logo size="xl" />
             </div>
 
-            <LoginForm />
+            <LoginForm requestErrors={error} submitForm={onLogin} />
+            {loading && <p>Login request...</p>}
 
             <div className={styles.divider}>OR</div>
 
